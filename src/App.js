fcd846ca4a52f596e9ccb7c8df88cf290ec4b475
Jason.Yang@wonderful.com <jason.yang@wonderful.com>
2019-07-29 22:01:36 -0700

    Refactor js, add transition animation for scrolled to sections

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './css/App.css';
-import main           from './js/main'
 import Contentful     from './components/contentful/contentful'
 import Tabs           from './components/tabs/tabs';
 import Movies         from './components/movies/movies';
@@ -21,10 +20,74 @@ class Header extends Component {
     Contentful.headerBuilder()
   }
 
+  handleClick() {
+    var colorArr = [
+      "255, 152, 0",    //Orange
+      "102, 187, 106",  //Green
+      "100, 124, 100",  //Army
+      "176, 190, 197",  //Silver
+      "255, 138, 128",  //Pink
+      "71, 172, 255",   //Blue
+      "52, 152, 219",   //Blue (Peterriver)
+      "44, 62, 80",     //Midnight Blue
+      "26, 188, 156",   //Turquoise
+      "154, 0, 7",      //Burgundy
+      "244, 67, 54",    //Red
+      "0, 188, 212",    //Teal
+      "103, 58, 183",   //Purple
+      "96, 125, 139"    //Steel
+    ];
+
+    var randomNum = Math.round(Math.random() * 9);
+    var randomColor = colorArr[randomNum];
+
+    // Set ID so we can check if page is currently using that color, if it is re-run the randomizer
+    var bodyElement = document.getElementById('top')
+    bodyElement.setAttribute("primary-color-id", randomNum);
+
+    while (bodyElement.getAttribute("primary-color-id") === randomNum) {
+      randomNum = Math.round(Math.random() * 9);
+      randomColor = colorArr[randomNum];
+    }
+
+    var setStyle = "";
+    var el = "";
+    var i = 0;
+
+    /* change background colors */
+    setStyle = "background-color: rgba(" + randomColor + ", 0.85 )";
+    el = document.querySelectorAll("header#header, .myskillicons .caption, footer#footer, .material-bar");
+    for (i = 0; i < el.length; i++) {
+      el[i].setAttribute("style", setStyle);
+    }
+
+    /* change border top color */
+    setStyle = "border-top: 2px solid rgba(" + randomColor + ", 0.5 )";
+    el = document.querySelectorAll("#main > section");
+    for (i = 0; i < el.length; i++) {
+      el[i].setAttribute("style", setStyle);
+    }
+
+    /* change border bottom color */
+    setStyle = "border-bottom: 2px solid rgba(" + randomColor + ", 0.5 )";
+    el = document.querySelectorAll(".experience-card h5");
+    for (i = 0; i < el.length; i++) {
+      el[i].setAttribute("style", setStyle);
+    }
+
+    /* border color for tabs */
+    setStyle = "border-color: rgba(" + randomColor + ", 0.5 )";
+    el = document.querySelectorAll(".tabs .tab-list .tab-list-item");
+    for (i = 0; i < el.length; i++) {
+      el[i].setAttribute("style", setStyle);
+    }
+
+  }
+
   render() {
     return(
       <header id="header">
-        <div className="image avatar"><a href={"javascript:void(0);"} onClick={() => main.handleClick()}></a></div>
+        <div className="image avatar"><a href={"javascript:void(0);"} onClick={() => this.handleClick()}></a></div>
         <span className="material-bar"></span>
       </header>
      )
@@ -32,7 +95,6 @@ class Header extends Component {
 }
 
 class Introduction extends Component {
-
   componentDidMount() {
     Contentful.introductionBuilder()
   }
@@ -45,7 +107,6 @@ class Introduction extends Component {
 }
 
 class Skills extends Component {
-
   componentDidMount() {
     Contentful.mySkillsBuilder()
   }
@@ -94,7 +155,6 @@ class FavoriteBuilds extends Component {
 }
 
 class CodeSnippets extends Component {
-
   componentDidMount() {
     Contentful.codeSnippetsBuilder();
   }
@@ -191,21 +251,56 @@ class Loader extends Component {
 
 class App extends Component {
   componentDidMount() {
-    window.addEventListener('load', this.handleLoad);
- }
- handleLoad() {
-    setTimeout(function() {
+    this.addEventListeners();
+  }
+
+  handleLoad() {
+    setTimeout(function () {
       var elem = document.getElementById("loading-container");
       elem.classList.add("hidden");
-    }, 1000);
- }
+    }, 250);
+
+  }
+
+  addEventListeners = () => {
+    window.addEventListener('load', this.handleLoad);
+    window.addEventListener('load', this.animateSection);
+    window.addEventListener('scroll', this.animateSection, true);
+  }
+
+  removeEventListeners = () => {
+    window.removeEventListener('scroll', this.animateSection, true);
+  }
+
+  animateSection = () => {
+    let sectionEles = document.querySelectorAll('#main section');
+    
+    for (var i = 0; i < sectionEles.length; i++) {
+      if (this.isAnyPartOfElementInViewport(sectionEles[i])) {
+        sectionEles[i].classList.add('fade-in');
+      }
+    }
+
+  }
+
+  isAnyPartOfElementInViewport = (el) => {
+    const rect = el.getBoundingClientRect();
+    const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+    const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+
+    const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
+    const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
+
+    return (vertInView && horInView);
+  };
+
   render() {
     return (
       <div id="top" primary-color-id="">
-        <Loader/>
-        <Header/>
-        <Body/>
-        <Footer/>
+        <Loader />
+        <Header />
+        <Body />
+        <Footer />
       </div>
     );
   }
